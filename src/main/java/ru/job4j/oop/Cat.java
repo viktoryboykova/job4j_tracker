de91ca9c66e39242fa2dd5479a29a6971302a569
viktoryboykova <viktoryboykova@list.ru>
2021-10-04 22:08:01 +0300

    Поля объекта

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -2,16 +2,30 @@ package ru.job4j.oop;
 
 public class Cat {
 
-    public String sound() {
-        String voice = "mew-mew";
-        return voice;
+    private String food;
+    private String name;
+
+    public void nick(String name) {
+        this.name = name;
+    }
+
+    public void show() {
+        System.out.println("Это котик " + this.name + ". Он ест " +this.food);
+    }
+
+    public void eat(String food) {
+        this.food = food;
     }
 
         public static void main(String[] args) {
-            Cat peppy = new Cat();
-            Cat sparky = new Cat();
-            Cat kisaVasilev = new Cat();
-            String say = kisaVasilev.sound();
-            System.out.println("Kisa Vasilev says " + say);
+            Cat twix = new Cat();
+            twix.nick("Твикс");
+            twix.eat("Мнямс-палочки");
+            twix.show();
+            Cat kisa = new Cat();
+            kisa.nick("Кексик");
+            kisa.eat("Мнямс-подушечки");
+            kisa.show();
         }
+
 }
