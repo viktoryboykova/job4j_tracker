a15e436dd99a9a9af2d8eb39642060bb0f93d685
viktoryboykova <viktoryboykova@list.ru>
2021-10-04 20:48:03 +0300

    Вызов метода объекта

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -2,8 +2,14 @@ package ru.job4j.oop;
 
 public class Cat {
 
+    public void mew() {
+        System.out.println("Киса Васильев любит мяу-мяу-мяу");
+    }
+
         public static void main(String[] args) {
             Cat peppy = new Cat();
             Cat sparky = new Cat();
+            Cat kisaVasilev = new Cat();
+            kisaVasilev.mew();
         }
 }
