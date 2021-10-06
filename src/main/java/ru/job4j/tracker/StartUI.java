5dfac385c7c1ba41dbcec007ab950fac27be613a
viktoryboykova <viktoryboykova@list.ru>
2021-10-06 22:12:56 +0300

    toString

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,11 +6,12 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item();
-        System.out.println(item.getCreated());
+        System.out.println(item);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-yyyy HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = item.getCreated().format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+
 
     }
 }
