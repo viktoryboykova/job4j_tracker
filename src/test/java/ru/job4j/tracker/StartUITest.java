69612996eef948e0771310432c6994004939017c
viktoryboykova <viktoryboykova@list.ru>
2021-10-08 17:54:02 +0300

    Реализация меню за счет шаблона стратегия

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -9,25 +9,4 @@ import static org.junit.Assert.*;
 
 public class StartUITest {
 
-    @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-
-    @Test
-    public void whenAddItem1() {
-        String[] answers = {"Киса"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Киса");
-        assertThat(created.getName(), is(expected.getName()));
-    }
 }
--- src/test/java/ru/job4j/tracker/StartUITest1.java
+++ src/test/java/ru/job4j/tracker/StartUITest1.java
@@ -7,16 +7,4 @@ import static org.junit.Assert.*;
 
 public class StartUITest1 {
 
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("Киса");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()), "Киса-сосиса"
-        };
-        StartUI.replaceItem(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("Киса-сосиса"));
-    }
 }
