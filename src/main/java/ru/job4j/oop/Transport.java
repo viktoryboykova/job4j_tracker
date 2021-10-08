fbea6fb3994ca197ff04173e7be50ea2a72d12c7
viktoryboykova <viktoryboykova@list.ru>
2021-10-08 11:59:22 +0300

    Приведение типов

--- src/main/java/ru/job4j/oop/Transport.java
+++ src/main/java/ru/job4j/oop/Transport.java
@@ -1,4 +1,14 @@
 package ru.job4j.oop;
 
 public class Transport {
+    public static void main(String[] args) {
+        Vehicle airplane = new Airplane();
+        Vehicle train = new Train();
+        Vehicle bus = new Bus();
+
+        Vehicle[] vehicles = new Vehicle[]{airplane, train, bus};
+        for (Vehicle a : vehicles) {
+            a.move();
+        }
+    }
 }
