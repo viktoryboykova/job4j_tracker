3c92d3a99202cede7a96a6ca75be83ba5db0e73f
viktoryboykova <viktoryboykova@list.ru>
2021-10-19 14:41:42 +0300

    Коллекции, ArrayList, List, Обобщения

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class UsageArrayList {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>(); //спросить у Егора
+        ArrayList<String> names = new ArrayList<String>();
         names.add("Petr");
         names.add("Ivan");
         names.add("Stepan");
