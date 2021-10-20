367c5044b4189119732ee78cd2eefc10803cc8f9
viktoryboykova <viktoryboykova@list.ru>
2021-10-20 15:42:54 +0300

    Модели данных и HashSet

---
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class NotifyAccountTest {
+
+    @Test
+    public void sent() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("123", "duplicate", "v"),
+                new Account("142", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> result = NotifyAccount.sent(accounts);
+
+        HashSet<Account> expect = new HashSet<>();
+        expect.add(new Account("123", "Petr Arsentev", "eDer3432f"));
+        expect.add(new Account("142", "Petr Arsentev", "000001"));
+
+        assertThat(result, is(expect));
+    }
+}
