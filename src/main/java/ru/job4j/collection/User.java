2106a554b24f95f3e1b7e44d26b76d11a3f4c699
viktoryboykova <viktoryboykova@list.ru>
2021-10-21 15:55:47 +0300

    Организовать сортировку User

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,52 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int i = this.name.compareTo(o.name);
+        if (i == 0) {
+            i = Integer.compare(this.age, o.age);
+        }
+        return i;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
