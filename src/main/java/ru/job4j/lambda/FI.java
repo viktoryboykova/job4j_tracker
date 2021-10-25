0ed512e8cc4df0ea25b2c75c7a6b33daf10c3836
viktoryboykova <viktoryboykova@list.ru>
2021-10-25 17:43:30 +0300

    Лямбда + отдельное от Attachment задание

---
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -0,0 +1,25 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FI {
+    public static void main(String[] args) {
+        Attachment[] atts = {
+                new Attachment("image 1", 20),
+                new Attachment("image 3", 120),
+                new Attachment("image 2", 23)
+        };
+        Comparator<Attachment> comparator = new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment left, Attachment right) {
+                return left.getSize() - right.getSize();
+            }
+        };
+        Arrays.sort(atts, comparator);
+    }
+
+    Comparator<String> cmpText = (left, right) -> left.compareTo(right);
+
+    Comparator<String> cmpDescSize = (left, right) -> right.length() - left.length();
+}
