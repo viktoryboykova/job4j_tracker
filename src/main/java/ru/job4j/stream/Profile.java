41bd275a8c2f9a870e2f0b139078634ae024737e
viktoryboykova <viktoryboykova@list.ru>
2021-10-26 17:45:07 +0300

    Список адресов

---
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,17 @@
+package ru.job4j.stream;
+
+public class Profile {
+    private Address address;
+
+    public Profile(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+
+    @Test
+    public void getAdress() {
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Москва", "Черкизовская", 30, 90)),
+                new Profile(new Address("Балашиха", "Звездная", 8, 90)),
+                new Profile(new Address("Волгоград", "Гороховцев", 4, 90))
+        );
+        Profiles prof = new Profiles();
+        List<Address> rsl = prof.collect(profiles);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Москва", "Черкизовская", 30, 90));
+        expected.add(new Address("Балашиха", "Звездная", 8, 90));
+        expected.add(new Address("Волгоград", "Гороховцев", 4, 90));
+    }
+}
